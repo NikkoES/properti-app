d5301b31278177a5c2e92a1b1bdfe7f534b0a633
NikkoES <nikkoeka04@gmail.com>
2018-07-09 00:39:16 +0700

    fix layout unseen

--- app/src/main/java/com/analisaproperti/analisaproperti/activity/BantuanActivity.java
+++ app/src/main/java/com/analisaproperti/analisaproperti/activity/BantuanActivity.java
@@ -66,7 +66,7 @@ public class BantuanActivity extends AppCompatActivity {
             getSupportActionBar().setTitle(getString(R.string.nav_main));
             layoutPendahuluan.setVisibility(View.VISIBLE);
             layoutBantuan.setVisibility(View.GONE);
-            setPendahuluan();
+            setPendahuluan(pref.getString("language", ""));
         }
         else{
             getSupportActionBar().setTitle(getString(R.string.nav_bantuan));
@@ -74,21 +74,40 @@ public class BantuanActivity extends AppCompatActivity {
         }
     }
 
-    private void setPendahuluan(){
-        String pendahuluan = "<b>Pendahuluan :</b><br>\n" +
-                "Apps ini dibuat untuk mempermudah semua kalangan membeli property dimana ada beberapa tahapan :<br>\n" +
-                "1. Hitungan Cicilan ke bank<br>\n" +
-                "2. Perbandingan Harga Property<br>\n" +
-                "3. Hitungan Income setiap bulan<br><br>\n" +
-                "<b>Hitungan Cicilan ke Bank,</b><br>\n" +
-                "Kita dapat mengetaui berapa besaran Cicilan per bulan yang harus di bayarkan ke Bank dan kemampuan kita untuk cicil<br><br>\n" +
-                "<b>Perbandingan harga Property,</b><br>\n" +
-                "Kita pembeli property harus mengetahui target property yang mau dibeli oleh kita apakah harganya dibawah pasaran atau harganya sangat mahal, untuk itu kita perlu alat perbandingan untuk mempermudah cara beli<br><br>\n" +
-                "<b>Hitungan Income setiap bulan,</b><br>\n" +
-                "Hitungan ini untuk mempermudah hitungan jika kita membeli property dengan income perbulan apakah sudah bisa menutupi cicilan kebank<br><br>\n" +
-                "Semoga dengan apps ini dapat membantu seseorang lebih teliti dalam membeli property.<br><br>\n" +
-                "Sukses selalu, <br>\n" +
-                "“see you on the top”";
+    private void setPendahuluan(String language){
+        String pendahuluan = "";
+        if(language.equals("en")){
+            pendahuluan = "<b>Preface :</b><br>\n" +
+                    "This app is made to make everyone can handle their property easier in some steps :<br>\n" +
+                    "1. Bank’s installment calculation<br>\n" +
+                    "2. Property’s price comparison<br>\n" +
+                    "3. Monthly income calculation<br><br>\n" +
+                    "<b>Bank’s Instalment Calculation</b><br>\n" +
+                    "We can know how many the installment calculation that we should pay to the bank every month and to measure our capability to pay.<br><br>\n" +
+                    "<b>Property’s Price Comparison</b><br>\n" +
+                    "We should know our property’s target price wether  it is too expensive or too cheap in the market.so that we need the comparison tool to give us the wiser insight.<br><br>\n" +
+                    "<b>Monthly Income Calculation</b><br>\n" +
+                    "This feature will help us to calculate our capability to pay the installment. Can our monthly income cover the installment or not? Therefore, it is important to be used.<br><br>\n" +
+                    "We hope this apps will help you a lot in the property business and make everyone buys the property more carefully.<br><br>\n" +
+                    "Warm regards,<br>\n" +
+                    "“see you on the top”";
+        }
+        else{
+            pendahuluan = "<b>Pendahuluan :</b><br>\n" +
+                    "Apps ini dibuat untuk mempermudah semua kalangan membeli property dimana ada beberapa tahapan :<br>\n" +
+                    "1. Hitungan Cicilan ke bank<br>\n" +
+                    "2. Perbandingan Harga Property<br>\n" +
+                    "3. Hitungan Income setiap bulan<br><br>\n" +
+                    "<b>Hitungan Cicilan ke Bank</b><br>\n" +
+                    "Kita dapat mengetaui berapa besaran Cicilan per bulan yang harus di bayarkan ke Bank dan kemampuan kita untuk cicil.<br><br>\n" +
+                    "<b>Perbandingan harga Property</b><br>\n" +
+                    "Kita pembeli property harus mengetahui target property yang mau dibeli oleh kita apakah harganya dibawah pasaran atau harganya sangat mahal, untuk itu kita perlu alat perbandingan untuk mempermudah cara beli.<br><br>\n" +
+                    "<b>Hitungan Income setiap bulan</b><br>\n" +
+                    "Hitungan ini untuk mempermudah hitungan jika kita membeli property dengan income perbulan apakah sudah bisa menutupi cicilan kebank.<br><br>\n" +
+                    "Semoga dengan apps ini dapat membantu seseorang lebih teliti dalam membeli property.<br><br>\n" +
+                    "Sukses selalu, <br>\n" +
+                    "“see you on the top”";
+        }
         txtPendahuluan.setText(Html.fromHtml(pendahuluan));
     }
 
